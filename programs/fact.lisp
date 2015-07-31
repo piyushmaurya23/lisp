e4ae0b07273e8014f380fb22882c9d9032d224b5
Piyush Maurya <piyush.maurya@live.in>
2015-07-31 20:42:11 +0530

    comments

--- programs/fact.lisp
+++ programs/fact.lisp
@@ -1,3 +1,5 @@
+;;;; Program No. 4: Define a Recursive LISP function to compute factorial of a given number.
+
 (defun fact(n)
 	(if (= n 1)
 		1
--- programs/square_sum.lisp
+++ programs/square_sum.lisp
@@ -1,2 +1,4 @@
+;;;; Program No.4: Define a LISP function to compute sum of squares.
+
 (defun square_sum(x y)
 	(+ (* x x) (* y y)))
