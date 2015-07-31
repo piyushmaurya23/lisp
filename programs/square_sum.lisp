9577a745991fec39239b391c5c3871e86282ee88
Piyush Maurya <piyush.maurya@live.in>
2015-07-31 20:59:04 +0530

    Update square_sum.lisp

--- programs/square_sum.lisp
+++ programs/square_sum.lisp
@@ -1,4 +1,4 @@
-;;;; Program No.4: Define a LISP function to compute sum of squares.
+;;;; Program No.1: Define a LISP function to compute sum of squares.
 
 (defun square_sum(x y)
 	(+ (* x x) (* y y)))
