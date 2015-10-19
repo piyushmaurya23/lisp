7ff249246e03a26f79808c88f0086eee857c053d
Piyush Maurya <piyush.maurya@live.in>
2015-10-19 17:57:40 +0530

    condition added

--- programs/fact.lisp
+++ programs/fact.lisp
@@ -1,4 +1,4 @@
-;;;; Program No. 4: Define a Recursive LISP function to compute factorial of a given number.
+;;;; Program No. 4: Define a Recursive LISP function to compute factorial of a given number(n>0).
 
 (defun fact(n)
 	(if (= n 1)
